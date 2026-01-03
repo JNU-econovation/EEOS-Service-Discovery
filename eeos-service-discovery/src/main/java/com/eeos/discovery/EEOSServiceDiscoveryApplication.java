08837f73719d5a052f73c5180231c12642b04dbd
Bellmin <whdals011203@naver.com>
2026-01-03 15:16:42 +0900

    chore: 프로젝트 초기 세팅

---
+++ eeos-service-discovery/src/main/java/com/eeos/discovery/EEOSServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.eeos.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EEOSServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EEOSServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ eeos-service-discovery/src/test/java/com/eeos/discovery/EeosServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeos.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeosServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
